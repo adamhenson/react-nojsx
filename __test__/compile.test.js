cc798b54341a5ac994f89c1a71a1f7d15ea1a6c4
adamhenson <adamhenson1979@gmail.com>
2017-01-06 11:01:21 -0500

    second test case.

--- __test__/compile.test.js
+++ __test__/compile.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NoJSX from '../src';
 import { mount } from 'enzyme';
 
-test('compile() creates a new "div" React element with only text content (children).', () => {
+test('compile() creates a new "div" React element type with text "children".', () => {
   const templateData = {
     children: 'hello world.',
     type: 'div'
@@ -12,6 +12,23 @@ test('compile() creates a new "div" React element with only text content (childr
   const newReactElement = template.compile();
   const wrapper = mount(newReactElement);
   const div = wrapper.find('div');
+
+  expect(div.text()).toBe('hello world.');
+});
+
+test('compile() creates a new "div" React element type with text "children" and "props".', () => {
+  const templateData = {
+    children: 'hello world.',
+    props: {
+      className: 'foobar'
+    },
+    type: 'div'
+  };
+
+  const template = new NoJSX(templateData);
+  const newReactElement = template.compile();
+  const wrapper = mount(newReactElement);
+  const div = wrapper.find('.foobar');
 
   expect(div.text()).toBe('hello world.');
 });
