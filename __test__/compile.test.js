f47fe3b7b3666315358f1f2ae4406bd48e7e547e
adamhenson <adamhenson1979@gmail.com>
2017-01-06 14:13:13 -0500

    use snapshots in testing.

--- __test__/compile.test.js
+++ __test__/compile.test.js
@@ -1,81 +1,74 @@
 import React from 'react';
 import NoJSX from '../src';
-import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 
-test('compile() creates a new "div" React element type with text "children".', () => {
-  const templateData = {
-    children: 'hello world.',
-    type: 'div'
-  };
-
+const render = (templateData) => {
   const template = new NoJSX(templateData);
   const newReactElement = template.compile();
-  const wrapper = mount(newReactElement);
-  const div = wrapper.find('div');
+  
+  return renderer.create(newReactElement);
+};
 
-  expect(div.text()).toBe('hello world.');
-});
+describe('compile() creates elements correctly.', () => {
+  it('creates a new "div" React element type with text "children".', () => {
+    const templateData = {
+      children: 'hello world.',
+      type: 'div'
+    };
 
-test('compile() creates a new "div" React element type with text "children" and "props".', () => {
-  const templateData = {
-    children: 'hello world.',
-    props: {
-      className: 'foobar'
-    },
-    type: 'div'
-  };
+    const rendered = render(templateData);
+    expect(rendered.toJSON()).toMatchSnapshot();
+  });
 
-  const template = new NoJSX(templateData);
-  const newReactElement = template.compile();
-  const wrapper = mount(newReactElement);
-  const div = wrapper.find('.foobar');
+  it('creates a new "div" React element type with text "children" and "props".', () => {
+    const templateData = {
+      children: 'hello world.',
+      props: {
+        className: 'foobar'
+      },
+      type: 'div'
+    };
 
-  expect(div.text()).toBe('hello world.');
-});
+    const rendered = render(templateData);
+    expect(rendered.toJSON()).toMatchSnapshot();
+  });
 
-test('compile() creates a new "div" React element and a child element defined in "children"... and "props".', () => {
-  const templateData = {
-    children: [
-      {
-        children: 'hello world.',
-        props: {
-          className: 'bar'
-        },
-        type: 'div'
+  it('creates a new "div" React element and a child element defined in "children"... and "props".', () => {
+    const templateData = {
+      children: [
+        {
+          children: 'hello world.',
+          props: {
+            className: 'bar'
+          },
+          type: 'div'
+        }
+      ],
+      props: {
+        className: 'foo'
+      },
+      type: 'div'
+    };
+
+    const rendered = render(templateData);
+    expect(rendered.toJSON()).toMatchSnapshot();
+  });
+
+  it('creates a new custom React component element type with "props".', () => {
+    class Greeting extends React.Component {
+      render() {
+        return <div className={ this.props.className }>hello world.</div>;
       }
-    ],
-    props: {
-      className: 'foo'
-    },
-    type: 'div'
-  };
-
-  const template = new NoJSX(templateData);
-  const newReactElement = template.compile();
-  const wrapper = mount(newReactElement);
-  const div = wrapper.find('.foo .bar');
-
-  expect(div.text()).toBe('hello world.');
-});
-
-test('compile() creates a new custom React component element type with "props".', () => {
-  class Greeting extends React.Component {
-    render() {
-      return <div className={ this.props.className }>hello world.</div>;
     }
-  }
 
-  const templateData = {
-    props: {
-      className: 'foobar'
-    },
-    type: Greeting
-  };
+    const templateData = {
+      props: {
+        className: 'foobar'
+      },
+      type: Greeting
+    };
 
-  const template = new NoJSX(templateData);
-  const newReactElement = template.compile();
-  const wrapper = mount(newReactElement);
-  const div = wrapper.find('.foobar');
-
-  expect(div.text()).toBe('hello world.');
+    const rendered = render(templateData);
+    expect(rendered.toJSON()).toMatchSnapshot();
+  });
 });
